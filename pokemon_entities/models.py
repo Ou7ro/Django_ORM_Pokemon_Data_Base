f0eeca19ddb6d17f9353a44e969e9f1c79da6381
Ou7ro <qew.qweqwe.2016@mail.ru>
2025-03-22 17:40:43 +0500

    Make the fields non-optional

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,8 +3,8 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     title = models.CharField('Название на русском', max_length=200)
-    title_eng = models.CharField('Название на английском', max_length=200)
-    title_jp = models.CharField('Название на японском', max_length=200)
+    title_eng = models.CharField('Название на английском', max_length=200, blank=True)
+    title_jp = models.CharField('Название на японском', max_length=200, blank=True)
     image = models.ImageField('Изображение', null=True, blank=True)
     description = models.TextField('Описание', blank=True)
     previous_evolution = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True,
@@ -16,10 +16,10 @@ class Pokemon(models.Model):
 
 class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name='Покемон')
-    lat = models.FloatField('Широта')
-    lon = models.FloatField('Долгота')
-    appeared_at = models.DateTimeField('Время появления', null=True)
-    disappeared_at = models.DateTimeField('Время исчезания', null=True)
+    lat = models.FloatField('Широта', blank=True, null=True)
+    lon = models.FloatField('Долгота', blank=True, null=True)
+    appeared_at = models.DateTimeField('Время появления', blank=True, null=True)
+    disappeared_at = models.DateTimeField('Время исчезания', blank=True, null=True)
     level = models.IntegerField('Уровень', blank=True, null=True)
     health = models.IntegerField('Здоровье', blank=True, null=True)
     strength = models.IntegerField('Атака', blank=True, null=True)
