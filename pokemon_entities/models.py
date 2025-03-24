93bc3572e69a9ee9d570df250ad21046501aa702
Ou7ro <qew.qweqwe.2016@mail.ru>
2025-03-24 21:42:23 +0500

    pointed out the related_name

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -8,14 +8,14 @@ class Pokemon(models.Model):
     image = models.ImageField('Изображение', upload_to='pokemon_images', null=True, blank=True)
     description = models.TextField('Описание', blank=True)
     previous_evolution = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True,
-                                           related_name="next_evolutions", verbose_name="Предыдущая эволюция")
+                                           related_name='next_evolutions', verbose_name='Предыдущая эволюция')
 
     def __str__(self):
         return self.title
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name='Покемон')
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, related_name='entities', verbose_name='Покемон')
     lat = models.FloatField('Широта', blank=True, null=True)
     lon = models.FloatField('Долгота', blank=True, null=True)
     appeared_at = models.DateTimeField('Время появления', blank=True, null=True)
